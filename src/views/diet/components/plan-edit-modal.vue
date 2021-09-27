ffc49bc4acf0d0242beeec4c4ebb4c76fe7c1cb4
Lokep <iamlokep@163.com>
2021-09-27 19:19:28 +0800

    feature: 饮食计划

--- src/views/diet/components/plan-edit-modal.vue
+++ src/views/diet/components/plan-edit-modal.vue
@@ -24,16 +24,17 @@
       <div class="flex dialog-form">
         <div class="dialog-form__el" style="flex: 1">
           <DietPlanForm
-            v-for="(item, index) in ruleList"
+            v-for="(item, index) in form.ruleList"
             :key="index"
             class="mb-5"
           >
             <template slot="title">
-              <div>第{{ item.dayNum }}天{{ index + 1 }}</div>
-              <div>热量：400 千卡</div>
+              <div>第{{ item.dayNum }}天 </div>
+              <!-- <div>热量：400 千卡</div>
               <div>蛋白：2.7 克</div>
               <div>脂肪：3.0 克</div>
-              <div>碳水：0.7 克</div>
+              <div>碳水：0.7 克</div> -->
+              <div>{{ item.dayList | handleDayContainerFilter(foodList) }}</div>
             </template>
 
             <div class="dialog-form-list">
@@ -43,9 +44,8 @@
                 class="dialog-form-item"
               >
                 <el-form-item :label="it.meal" label-width="80px" required>
-
                   <el-button
-                    v-if="it.detailList.length == 0"
+                    v-if="it.detailList.length === 0"
                     type="primary"
                     size="mini"
                     icon="el-icon-plus"
@@ -61,11 +61,13 @@
                     style="margin-bottom: 10px"
                   >
                     <div class="justify-between flex">
+
+                      <!-- 选择食物分类 -->
                       <el-select
-                        v-model="el.categoryId"
+                        :value="el.categoryId"
                         size="mini"
                         autocomplete="off"
-                        @change="foodCategoryChange($event)"
+                        @change="e => foodCategoryChange(e, index, i, idx, 'categoryId')"
                       >
                         <el-option
                           v-for="categoryItem in categorytList"
@@ -74,10 +76,13 @@
                           :value="categoryItem.id"
                         />
                       </el-select>
+
+                      <!-- 选择食物 -->
                       <el-select
-                        v-model="el.foodId"
+                        :value="el.foodId"
                         size="mini"
                         autocomplete="off"
+                        @change="e => handleFoodChange(e, index, i, idx, 'foodId')"
                       >
                         <el-option
                           v-for="categoryItem in foodList"
@@ -86,26 +91,37 @@
                           :value="categoryItem.id"
                         />
                       </el-select>
+
+                      <!-- 食物数量 -->
                       <el-input-number
-                        v-model="el.nums"
+                        :value="el.nums"
                         size="mini"
                         :min="1"
                         :max="9999"
                         label="描述文字"
+                        @change="e => handleInputChange(e, index, i, idx, 'nums')"
                       />
-                      杯
+
+                      {{ el.unit }}
+
+                      <!-- 操作按钮 -->
                       <i
                         class="el-icon-circle-plus dialog-form__el"
                         @click="addDetailListItem(index, i, idx)"
                       />
-                      <i class="el-icon-error dialog-form__el" />
+                      <i
+                        v-if="idx > 0"
+                        class="el-icon-error dialog-form__el"
+                        @click="deleteDetailListItem(index, i, idx)"
+                      />
+
+                      <!-- ---- -->
                     </div>
                     <div class="dialog-form-item__desc">
-                      热量：400 千卡 蛋白：2.7 克 脂肪：3.0 克 碳水：0.7 克
+                      {{ el | handleContainFilter(foodList) }}
                     </div>
                   </div>
                 </el-form-item>
-
               </div>
             </div>
 
@@ -126,20 +142,52 @@
         </div>
       </div>
     </el-form>
-    <div slot="footer" class="dialog-footer">
-      <el-button
-        size="mini"
-        @click="$emit('update:dialogFormVisible', false)"
-      >取 消</el-button>
-      <el-button size="mini" type="primary" @click="handleEditForm">
+    <span slot="footer" class="dialog-footer mt-10" style="display: flex; justify-content: flex-end;">
+      <el-button size="mini" @click="e => handleClose()">取 消</el-button>
+      <el-button size="mini" type="primary" @click="addDietPlan">
         确 定
       </el-button>
-    </div>
+    </span>
   </div>
 </template>
 
 <script>
 import { getFoodCategorytList, getFoodSelectList } from '@/api/food'
+import { addDietPlan } from '@/api/diet'
+import { deepClone } from '@/utils'
+
+const FORM_SKELETON = {
+  'createTime': 0,
+  'creatorId': '',
+  'creatorName': '',
+  'id': '',
+  'isDelete': 0,
+  'receiveNums': 0,
+  'planName': '',
+  'dayCount': 1,
+  'ruleList': [
+    {
+      'dayNum': 1,
+      'dayList': [
+        {
+          'mealType': 1,
+          'meal': '早餐',
+          'detailList': []
+        },
+        {
+          'mealType': 2,
+          'meal': '午餐',
+          'detailList': []
+        },
+        {
+          'mealType': 3,
+          'meal': '晚餐',
+          'detailList': []
+        }
+      ]
+    }
+  ]
+}
 
 const RULE_LIST_ITEM = {
   dayNum: 1,
@@ -158,14 +206,13 @@ const RULE_LIST_ITEM = {
       mealType: 3,
       meal: '晚餐',
       detailList: []
-    },
-    {
-      mealType: 4,
-      meal: '加餐',
-      detailList: []
     }
+    // {
+    //   mealType: 4,
+    //   meal: "加餐",
+    //   detailList: [],
+    // },
   ]
-
 }
 const DETAIL_LIST_ITEM = {
   categoryId: '',
@@ -176,6 +223,33 @@ const DETAIL_LIST_ITEM = {
 export default {
   components: {
     DietPlanForm: () => import('./diet-plan-form.vue')
+  },
+  filters: {
+    handleContainFilter: function(row = {}, list = []) {
+      if (row.foodId && row.nums >= 1) {
+        const { heat = 0, protein = 0, fat = 0, carbonWater = 0 } = list.find(item => item.id === row.foodId) || {}
+        return `热量：${heat * row.nums} 千卡 蛋白：${protein * row.nums} 克 脂肪：${fat * row.nums} 克 碳水：${carbonWater * row.nums} 克`
+      }
+    },
+    handleDayContainerFilter(list, foods) {
+      let heats = 0; let proteins = 0; let fats = 0; let carbonWaters = 0
+
+      if (foods && foods.length > 0 && list && list.length > 0) {
+        list.map(item => {
+          if (item.detailList && item.detailList.length > 0) {
+            item.detailList.map(it => {
+              const { heat = 0, protein = 0, fat = 0, carbonWater = 0 } = foods.find(el => el.id === it.foodId) || {}
+              heats += it.nums * heat
+              proteins += it.nums * protein
+              fats += it.nums * fat
+              carbonWaters += it.nums * carbonWater
+            })
+          }
+        })
+      }
+
+      return `热量：${heats} 千卡 蛋白：${proteins} 克 脂肪：${fats} 克 碳水：${carbonWaters} 克`
+    }
   },
   props: {
     form: {
@@ -193,26 +267,90 @@ export default {
     }
   },
   created() {
-    this.splitForm()
+    // this.splitForm();
     this.getFoodCategorytList()
   },
   methods: {
-    handleEditForm() {},
-    addItem(index) {
+    handleClose(refresh = false) {
+      this.$emit('update', {
+        dialogFormVisible: false,
+        form: FORM_SKELETON,
+        refresh
+      })
+    },
+
+    handleInputChange(e, index, i, idx, key) {
+      const form = deepClone(this.form)
+
+      form.ruleList[index].dayList[i].detailList[idx][key] = e
+
+      this.$emit('update', {
+        dialogFormVisible: true,
+        form,
+        refresh: false
+      })
+    },
+
+    handleFoodChange(e, index, i, idx) {
+      const form = deepClone(this.form)
+      const { unit = '' } = this.foodList.find(item => item.id === e) || {}
+      const item = form.ruleList[index].dayList[i].detailList[idx]
+
+      form.ruleList[index].dayList[i].detailList[idx] = {
+        ...item,
+        foodId: e,
+        unit,
+        nums: 1
+      }
+
+      this.$emit('update', {
+        dialogFormVisible: true,
+        form,
+        refresh: false
+      })
+    },
+
+    initRow(value, index, i, idx) {
+      const form = deepClone(this.form)
+      const item = form.ruleList[index].dayList[i].detailList[idx]
+
+      form.ruleList[index].dayList[i].detailList[idx] = {
+        ...item,
+        categoryId: value,
+        foodId: '',
+        unit: '',
+        nums: 1
+      }
+
+      this.$emit('update', {
+        dialogFormVisible: true,
+        form,
+        refresh: false
+      })
+    },
+
+    addItem() {
       /* 新增计划第几天 */
       this.form.ruleList.push(RULE_LIST_ITEM)
     },
 
-    /**
-     * 分解传入的form
-     */
-    splitForm() {
-      const { ruleList } = this.form
-      this.ruleList = ruleList
+    addDetailListItem(index, i, idx) {
+      this.form.ruleList[index].dayList[i].detailList.splice(
+        idx,
+        0,
+        DETAIL_LIST_ITEM
+      )
     },
 
-    addDetailListItem(index, i, idx) {
-      this.ruleList[index].dayList[i].detailList.splice(idx, 0, DETAIL_LIST_ITEM)
+    delItemHandle(index) {
+      this.form.ruleList.splice(index, 1)
+    },
+
+    deleteDetailListItem(index, i, idx) {
+      this.form.ruleList[index].dayList[i].detailList.splice(
+        idx,
+        1
+      )
     },
 
     /* 食物分类 */
@@ -220,26 +358,111 @@ export default {
       getFoodCategorytList({
         categoryType: 1
       }).then((res) => {
-        this.categorytList = res.data.list
+        this.categorytList = res.data
       })
     },
 
     /* 食物分类下拉改变时,ps: 应该要加参数 */
-    foodCategoryChange(value) {
+    foodCategoryChange(value, index, i, idx, key) {
       this.getFoodSelectList(value)
+      this.handleInputChange(value, index, i, idx, key)
+      this.initRow(value, index, i, idx)
     },
+
     /* 食物下拉列表 */
     getFoodSelectList(categoryId) {
       getFoodSelectList({ categoryId }).then((res) => {
-        this.foodList = res.data.list
+        this.foodList = res.data
       })
+    },
+
+    /** 新增 */
+    addDietPlan() {
+      const ruleList = this.makeUpRuleList()
+
+      const { status, message } = this.checkIsForm()
+
+      if (status === 'FAIL') {
+        this.$message.warning(message)
+        return
+      }
+
+      this.$confirm('是否确认执行此操作', '提示')
+        .then(() => {
+          addDietPlan({
+            ...this.form,
+            ruleList
+          }).then((res) => {
+            if (res.result === 1) {
+              this.$message.success('操作成功')
+              this.handleClose(true)
+            }
+          })
+        })
+        .catch(() => {})
+    },
+
+    checkIsForm() {
+      const { planName, ruleList } = this.form
+
+      if (!planName || planName.length > 20) {
+        return { status: 'FAIL', message: '请正确填写计划名称' }
+      }
+
+      for (let i = 0; i < ruleList.length; i++) {
+        const item = ruleList[i]
+        for (let m = 0; m < item.dayList.length; m++) {
+          const it = item.dayList[m]
+
+          if (it.detailList.length === 0) {
+            return { status: 'FAIL', message: '请完整设置饮食计划' }
+          }
+
+          for (let n = 0; n < it.detailList.length; n++) {
+            const el = it.detailList[n]
+
+            if (el.categoryId && el.foodId && el.nums) {
+              continue
+            } else {
+              if (!el.categoryId) {
+                return { status: 'FAIL', message: '请选择食物分类' }
+              } else if (!el.foodId) {
+                return { status: 'FAIL', message: '请选择食物' }
+              } else if (!el.nums) {
+                return { status: 'FAIL', message: '请选择食物数量' }
+              }
+            }
+          }
+        }
+      }
+
+      return { status: 'SUCCESS' }
+    },
+
+    makeUpRuleList() {
+      const ruleList = []
+      this.form.ruleList.map((item) => {
+        item.dayList.map(it => {
+          ruleList.push({
+            ...item,
+            // dayNum: item.dayNum,
+            detailList: it.detailList,
+            mealType: it.mealType
+            // id: item.id || null,
+            // planId: item.planId || null
+          })
+        })
+      })
+
+      return ruleList
     }
   }
+
 }
 </script>
 
 <style lang="scss" scoped>
-::v-deep .dialog-form{
+::v-deep .dialog-form {
   .el-input-number {
     margin-top: 3px;
   }
--- src/views/diet/plans/index.vue
+++ src/views/diet/plans/index.vue
@@ -76,7 +76,7 @@
         <el-table-column align="center" prop="receiveNums" label="领取人次" />
         <el-table-column align="center" label="操作">
           <template slot-scope="{ row }">
-            <el-button type="text" size="mini">编辑</el-button>
+            <el-button type="text" size="mini" @click="handleEdit(row)">编辑</el-button>
             <el-button
               type="text"
               size="mini"
@@ -101,19 +101,20 @@
     <el-dialog
       :title="form.id ? '编辑饮食计划' : '新建饮食计划'"
       :visible.sync="dialogFormVisible"
-      width="768px"
+      width="725px"
     >
-      <plan-edit-modal :form="form" />
+      <plan-edit-modal :form="form" @update="handleFormUpdate" />
     </el-dialog>
   </div>
 </template>
 
 <script>
 import user from '@/mixin/user'
-import { deepClone, parseTime } from '@/utils'
-import { getPlanList, deletePlan } from '@/api/diet'
+import { deepClone } from '@/utils'
+import { getPlanList, deleteDietPlan, getDietPlanInfo } from '@/api/diet'
 import { getFoodCategorytList, getFoodSelectList } from '@/api/food'
-import { getDishCategoryList } from '@/api/diet'
+
+import * as dayjs from 'dayjs'
 
 import PlanEditModal from '../components/plan-edit-modal.vue'
 
@@ -123,14 +124,15 @@ export default {
   },
   filters: {
     parseTime(v) {
-      return parseTime(v, '{y}-{m}-{d}')
+      return dayjs(v).format('YYYY-MM-DD hh:mm:ss')
     }
   },
   /* mixins 用户权限 备用 */
   mixins: [user],
   data() {
     return {
-      dialogFormVisible: true,
+      dialogFormVisible: false,
+      isEdit: false,
       params: {
         pageNum: 1,
         pageSize: 10,
@@ -166,12 +168,12 @@ export default {
                 mealType: 3,
                 meal: '晚餐',
                 detailList: []
-              },
-              {
-                mealType: 4,
-                meal: '加餐',
-                detailList: []
               }
+              // {
+              //   mealType: 4,
+              //   meal: '加餐',
+              //   detailList: []
+              // }
             ]
           }
         ]
@@ -207,11 +209,20 @@ export default {
     }
   },
   created() {
-    this.getDishCategoryList()
     this.getPlanList()
     this.getFoodCategorytList()
   },
   methods: {
+    handleEdit(row) {
+      const { id } = row
+      getDietPlanInfo({ id }).then(res => {
+        if (res.result === 1) {
+          this.form = handlePlan(res.plan)
+          this.isEdit = true
+          this.dialogFormVisible = true
+        }
+      })
+    },
     changePage(e) {
       this.current = e
       this.getPlanList({ pageNum: e })
@@ -239,7 +250,7 @@ export default {
       }
       this.$confirm('你确定要删除这条数据吗？', '提示', { type: 'warning' })
         .then(() => {
-          deletePlan({
+          deleteDietPlan({
             id
           }).then((res) => {
             if (res.result === 1) {
@@ -251,17 +262,6 @@ export default {
         .catch(() => {})
     },
 
-    updateForm(e) {
-      console.log(e)
-    },
-
-    getDishCategoryList() {
-      getDishCategoryList({
-        categoryType: 1
-      }).then((res) => {
-        console.log(res)
-      })
-    },
     /* 添加饮食计划 */
     creareHandle() {
       this.dialogFormVisible = true
@@ -284,7 +284,45 @@ export default {
       getFoodSelectList({ categoryId }).then((res) => {
         this.foodList = res.data.list
       })
+    },
+
+    handleFormUpdate(e) {
+      this.dialogFormVisible = e.dialogFormVisible
+      this.form = e.form
+      if (e.refresh) {
+        this.getPlanList()
+      }
     }
+  }
+}
+
+function handlePlan(plan) {
+  const result = []; const enum_mealType = ['', '早餐', '午餐', '晚餐']
+
+  plan.ruleList.map(item => {
+    const index = result.findIndex(el => el.dayNum === item.dayNum)
+
+    if (index === -1) {
+      result.push({
+        ...item,
+        dayList: [
+          {
+            ...item,
+            meal: enum_mealType[item.mealType]
+          }
+        ]
+      })
+    } else {
+      result[index].dayList.push({
+        ...item,
+        meal: enum_mealType[item.mealType]
+      })
+    }
+  })
+
+  return {
+    ...plan,
+    ruleList: result
   }
 }
 </script>
