08d8ced5b2c0a2b73595b516cf2a97b02eb1f6f8
fire <aslslgood22@163.com>
2021-09-27 21:29:21 +0800

    帐号查询删除

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -2,39 +2,57 @@
 <template>
   <div class="plans">
     <!-- 操作模板 -->
-    <div class="tools justify-between mb-10">
+    <div class="tools justify-center mb-10">
       <div class="tools-list align-center">
 
+        <div class="tools-item align-center">
+          <div class="tools-item__label">
+            帐号
+          </div>
+          <el-input
+            v-model="searchQuery.account"
+            size="mini"
+            placeholder="请输入内容"
+            prefix-icon="el-icon-search"
+            :clearable="true"
+          />
+        </div>
         <div class="tools-item align-center">
           <div class="tools-item__label">
             用户名
           </div>
           <el-input
-            v-model="input2"
+            v-model="searchQuery.name"
             size="mini"
             placeholder="请输入内容"
             prefix-icon="el-icon-search"
+            :clearable="true"
           />
         </div>
-
       </div>
-
       <div class="tools-btns align-center">
-        <el-button size="mini" type="success" icon="el-icon-search">查询</el-button>
+        <el-button size="mini" type="success" icon="el-icon-search" @click="getUserList">查询</el-button>
         <el-button size="mini" type="primary" icon="el-icon-plus">新建</el-button>
       </div>
+
     </div>
 
     <div class="content">
-      <el-table size="mini" border>
-        <el-table-column align="center" props="" label="序号" type="index" />
-        <el-table-column align="center" props="" label="用户名" />
+      <el-table size="mini" border :data="userList">
+        <el-table-column align="center" label="序号" type="index" />
+        <el-table-column align="center" props="account" label="帐号" />
+        <el-table-column align="center" props="username" label="用户名" />
         <el-table-column align="center" props="" label="创建人" />
-        <el-table-column align="center" props="" label="创建时间" />
+        <el-table-column align="center" props="rolename" label="角色名称" />
+        <el-table-column align="center" prop="" label="创建时间">
+          <template slot-scope="{ row }">
+            {{ row.createTime | parseTimeFilter }}
+          </template>
+        </el-table-column>
         <el-table-column align="center" props="" label="操作">
-          <template>
+          <template slot-scope="{ row }">
             <el-button type="text" size="mini">编辑</el-button>
-            <el-button type="text" size="mini">删除</el-button>
+            <el-button type="text" size="mini" style="color: #F56C6C;" @click="deleteUser(row.id)">删除</el-button>
             <el-button type="text" size="mini">重置密码</el-button>
           </template>
         </el-table-column>
@@ -44,7 +62,7 @@
         <el-pagination
           background
           layout="prev, pager, next"
-          :total="1000"
+          :total="total"
         />
       </div>
     </div>
@@ -82,15 +100,56 @@
 </template>
 
 <script>
+import user from '@/mixin/user'
+import * as dayjs from 'dayjs'
+import { getUserList, deleteUser } from '@/api/user'
 export default {
+  filters: {
+    parseTimeFilter(e) {
+      return dayjs(e).format('YYYY-MM-DD HH:mm:ss')
+    }
+  },
+  mixins: [user],
   data() {
     return {
+      total: 10,
       isShowEditModalVisible: false,
       isShowPasswordVisible: false,
       adminInfo: {
         name: '',
         password: ''
-      }
+      },
+      searchQuery: {
+        account: '',
+        name: ''
+      },
+      userList: [{ id: 10086 }]
+    }
+  },
+  created() {
+    this.getUserList()
+  },
+  methods: {
+    deleteUser(id) {
+      this.$confirm('是否确认删除？', '提示', {
+        type: 'warning'
+      }).then(() => {
+        deleteUser({ id }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '操作成功'
+          })
+          this.getUserList()
+        })
+      })
+    },
+    getUserList() {
+      getUserList({
+        ...this.searchQuery
+      }).then(res => {
+        this.userList = res.data
+        this.total = res.total
+      })
     }
   }
 }
